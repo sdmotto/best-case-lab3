0d879fdc239f8aac9eef6898e01639160243016b
sdmotto <119888788+sdmotto@users.noreply.github.com>
2024-11-11 17:06:08 -0600

    initialized pages and nav bar

---
+++ components/Nav.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/campbell">Campbell</router-link></li>
+        <li><router-link to="/alex">Alex</router-link></li>
+        <li><router-link to="/sam">Sam</router-link></li>
+        <li><router-link to="/ethan">Ethan</router-link></li>
+      </ul>
+    </nav>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,14 @@
 <template>
     <div>
+        <Nav />
       <NuxtPage />
     </div>
 </template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
