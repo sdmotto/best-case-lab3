da5522f2c322afe83ae8324bf3c166b0323721c6
sdmotto <119888788+sdmotto@users.noreply.github.com>
2024-11-12 20:25:49 -0600

    refactor

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,12 @@
 import { getCookie } from 'h3';
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
-  if (process.server) {
+  if (import.meta.server) {
     const jwt = await import('jsonwebtoken');
     const event = useRequestEvent();
     const token = getCookie(event, 'authToken');
 
     try {
-      // Verify the token with the server’s secret
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
       if (!decoded.verified && ['/protected', '/emails'].includes(to.path)) {
