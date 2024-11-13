e6f056fbb8776e9cb71b89911c9c5c4a0e4e2032
sdmotto <119888788+sdmotto@users.noreply.github.com>
2024-11-12 20:35:07 -0600

    refactored emails

--- pages/emails.vue
+++ pages/emails.vue
@@ -3,9 +3,10 @@
     <h1>Email Logs</h1>
     <ul v-if="emails.length > 0">
       <li v-for="(email, index) in emails" :key="index">
+        <p><strong>From:</strong> {{ email.from }}</p>
         <p><strong>To:</strong> {{ email.to }}</p>
         <p><strong>Subject:</strong> {{ email.subject }}</p>
-        <p><strong>Message:</strong> {{ email.text }}</p>
+        <p><strong>Message:</strong> {{ email.message }}</p>
       </li>
     </ul>
     <p v-else>No emails found.</p>
--- server/api/send-email.post.js
+++ server/api/send-email.post.js
@@ -15,31 +15,40 @@ export default defineEventHandler(async (event) => {
     },
   });
 
-  const newMessage = `This email is from: ${fromEmail}\n\n` + message;
-
   const mailOptions = {
     from: 'best.case.labs@gmail.com',
     to,
     subject,
-    text: newMessage,
+    text: formatEmail(),
   };
 
   try {
     await transporter.sendMail(mailOptions);
+    await saveEmail();
+    
+    return { success: true };
+  } catch (error) {
+    return { success: false };
+  }
 
+  function formatEmail() {
+    return `This email is sent from ${fromEmail}\n\n` + message;
+  }
+
+  async function saveEmail() {
     const filePath = path.resolve('db/emails.json');
 
     let emailLogs = [];
     const fileData = await fs.readFile(filePath, 'utf8');
     emailLogs = JSON.parse(fileData);
 
-    emailLogs.push(mailOptions);
+    emailLogs.push({
+      from: fromEmail,
+      to: to,
+      subject: subject,
+      message: message
+    });
 
     await fs.writeFile(filePath, JSON.stringify(emailLogs, null, 2));
-
-
-    return { success: true };
-  } catch (error) {
-    return { success: false };
   }
 });
